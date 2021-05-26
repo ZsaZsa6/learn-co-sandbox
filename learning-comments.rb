a3b4eeb0815e46c38f4895656d392dc9a1f603df
Ronda D Cowen <cowenhome5@gmail.com>
2021-05-26 20:25:33 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,42 @@
+# run_code_inside = true
+# puts "Code before if...end"
+# if run_code_inside
+#   puts "code inside"
+# end
+# puts "Code after if...end"
+chance_of_rain = 1 
+# puts "Let's go outside"
+# if chance_of_rain > 0.5
+#   puts "Pack an umbrella"
+# else
+#   puts "Enjoy the fine day!"
+# end
+# puts "Oh, and always wear sunscreen"
+ chance_of_rain = 1 
+# if chance_of_rain <= 0.25
+#   puts "Pack a sun shelter"
+#   elsif (chance_of_rain > 0.25 && chance_of_rain > 0.75)
+#   puts "Pack an umbrella"
+# else
+#   puts "Stay home and read Hegel"
+# end
+# puts "You know what year it is?"
+# this_year = Time.now.year
+# puts "Hey, it's not 2019" unless this_year == 2019
+
+# current_weather = "raining"
+
+# case current_weather
+#   when "sunny"
+#     puts "grab some sunscreen!"
+#   when "raining" 
+#     puts "grab an umbrella"
+#   when "snowing"
+#     puts "bundle up"
+# end
+
+# count = 0
+# while count < 10 do
+#   puts "I am the #{count}, I love to count!" # Work
+#   count = count + 1
+# end
